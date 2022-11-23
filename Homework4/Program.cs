e5a9bec9066dee40cea3904bb1368d65e8b8e46e
Akim <akzar67@mail.ru>
2022-11-23 07:52:09 +0300

    added _FOR_

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -5,14 +5,17 @@
 Console.Clear();
  Console.Write("Введите число: ");
  int n = Convert.ToInt32(Console.ReadLine());
- int i = 2;
+ //int i = 2;
   while ( n <= 1 )
 {
      Console.Write("Вы ошиблись!\nВведите число больше единицы: ");
      n = Convert.ToInt32(Console.ReadLine());    
 }
-  while ( i <= n )
-{
-     {Console.Write($"{i} ");
-     i = i + 2;}
-}
+//   while ( i <= n )
+// {
+//      {Console.Write($"{i} ");
+//      i += 2;}
+// }
+for ( int i = 2; i <= n; i+=2)
+          Console.Write($"{i} ") ;
+
